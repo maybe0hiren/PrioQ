80234d1311e0b566882ca6ccea62d2c5ab5aee94
Hiren <hirenwaghmare25@gmail.com>
2025-08-24 14:18:03 +0530

    switch and display tasks

--- linkedListJSON.cpp
+++ linkedListJSON.cpp
@@ -2,31 +2,36 @@
 #include <time.h>
 #include <fstream>
 #include "json.hpp"
+using json = nlohmann::json;
 
-class Task{
-    public: 
+struct content{
         std::string name;
         time_t deadlineRaw;
         std::string deadline;
         int type;
         float weight;
+};
+
+class Task{
+    public:
+        struct content data;        
         Task* next;
 
         Task(std::string name, time_t deadlineRaw, int type, float weight){
-            this->name = name;
-            this->deadlineRaw = deadlineRaw;
-            this->type = type;
-            this->weight = weight;
+            this->data.name = name;
+            this->data.deadlineRaw = deadlineRaw;
+            this->data.type = type;
+            this->data.weight = weight;
 
-            this->deadline = ctime(&deadlineRaw);
+            this->data.deadline = ctime(&deadlineRaw);
             this->next = nullptr;
         }
 };
 
 class LinkedList{
     public:
-        Task* head;
-        Task* tail;
+        Task* head = nullptr;
+        Task* tail = nullptr;
 
         void push_back(Task* newTask){
             if(!head){
@@ -36,5 +41,18 @@ class LinkedList{
             Task* temp = tail;
             temp->next = newTask;
             tail = newTask;
+        }
+        void taskSwitch(Task* task1, Task* task2){
+            content temp;
+            temp = task1->data;
+            task1->data = task2->data;
+            task2->data = temp;
+        }
+        void display(){
+            Task* temp = head;
+            while(temp != nullptr){
+                std::cout << temp->data.name << " -> ";
+                temp = temp->next;
+            }
         }
 };
